01675ee37645c51d4e9c7be9d4b89193ba88c39d
kevilin.rodrigues <kevilin.rodrigues@kinghost.com.br>
2023-04-04 00:50:11 -0300

    updates

--- src/pages/MeusDados/Form/Form.logical.js
+++ src/pages/MeusDados/Form/Form.logical.js
@@ -2,9 +2,7 @@ import { useState } from "react";
 import { collection, addDoc } from "firebase/firestore";
 import app from "../../../firebase/firebaseConfig";
 import { getFirestore } from "firebase/firestore";
-import { getDownloadURL, getStorage, ref, uploadBytes } from "firebase/storage";
 import { useUserAuth } from "../../../context/userAuthContext";
-const storage = getStorage(app);
 const db = getFirestore(app);
 
 export const HandleClick = () => {
@@ -13,12 +11,11 @@ export const HandleClick = () => {
   const [toSubmit, setToSubmit] = useState(false);
   const [redirect, setRedirect] = useState(false);
   const [inputs, setInputs] = useState({});
-  const [urlLink, setUrlLink] = useState([]);
 
   //Upload do post para o Firebase
   const firebaseAdd = async () => {
     try {
-      await addDoc(collection(db, "posts"), {
+      await addDoc(collection(db, "usuarios"), {
         inputs,
       });
     } catch (e) {
@@ -27,7 +24,7 @@ export const HandleClick = () => {
   };
 
   const handleSubmit = (e) => {
-    const validate = inputs.nome_usuario === "" || inputs.sobrenome_usuario === "" || inputs.endereco === "" || inputs.cidade === "" || inputs.cep === "" ||  inputs.telefone.length < 8 || inputs.titulo_post === "";
+    const validate = inputs.nome_usuario === "" || inputs.sobrenome_usuario === "" || inputs.endereco === "" || inputs.cidade === "" || inputs.cep === "" ||  inputs.telefone.length < 8;
     e.preventDefault();
     if (validate) {
       return alert("Revise todos os campos");
@@ -40,31 +37,14 @@ export const HandleClick = () => {
     firebaseAdd();
   };
 
-  //Upload das imagens para o Firebase
-  const onFileChange = async (e) => {
-    if (urlLink.length < 3) {
-      //lê arquivo
-      const file = e.target.files[0];
-      //Upload
-      const fileRef = ref(storage, `imagens-roupas/${file.name}`);
-      const upload = await uploadBytes(fileRef, file);
-      //Pega a URL para download
-      const urlParaDownload = await getDownloadURL(fileRef);
-      setUrlLink((prevUrlLink) => [...prevUrlLink, { urlLink: urlParaDownload, imageData: upload.ref.name }]);
-    } else {
-      return alert("Só é possivel inserir 3 imagens.");
-    }
-  };
-
   //Lê e salva os inputs
   const handleChange = (e) => {
     setInputs({
       ...inputs,
       [e.target.name]: e.target.value,
-      urlLink,
       usuario,
     });
   };
 
-  return { handleSubmit, handleChange, onFileChange, firebaseAdd, urlLink, toSubmit, redirect };
+  return { handleSubmit, handleChange, firebaseAdd, toSubmit, redirect };
 };
--- src/pages/MeusDados/Form/index.jsx
+++ src/pages/MeusDados/Form/index.jsx
@@ -1,28 +1,15 @@
 import React, { useState } from "react";
-import { Stack, Box, InputGroup, InputLeftAddon, Input, FormControl, FormLabel, Text, Icon, Button, Image, HStack } from "@chakra-ui/react";
-import { VStack, Spinner } from "@chakra-ui/react";
-import { MdCloudUpload } from "react-icons/md";
+import { Stack, Box, InputGroup, InputLeftAddon, Input, FormControl, FormLabel, Text, Button, HStack } from "@chakra-ui/react";
+import { Spinner } from "@chakra-ui/react";
 import InputComp from "../../../components/Input";
 import SelectComp from "../../../components/Select";
 import { HandleClick } from "./Form.logical";
-import { nanoid } from "nanoid";
 import Confetti from "../../../components/Confetti";
 import { Navigate } from "react-router-dom";
 
 const Form = () => {
   const [uploadStatus, setUploadStatus] = useState(false);
-  const { handleSubmit, handleChange, onFileChange, urlLink, toSubmit, redirect } = HandleClick();
-
-  const handleChanges = (e) => {
-    if (e.target.files.length === 0) {
-      return null;
-    }
-    onFileChange(e);
-    setUploadStatus(true);
-    setTimeout(() => {
-      setUploadStatus(false);
-    }, 4000);
-  };
+  const { handleSubmit, handleChange, toSubmit, redirect } = HandleClick();
 
   return (
     <Stack as="form" w="100%" p={{ md: "6", base: "0" }} spacing={6} onSubmit={handleSubmit}>
@@ -41,61 +28,21 @@ const Form = () => {
       </Box>
       <InputComp nameprop="cep" label="CEP" type="text" place="" value={handleChange} />
       <Stack direction={{ base: "column", md: "row" }}>
-      <SelectComp title="Cidade" options={["Bastante usada","Pouco usada", "Semi nova"]} name="cidade" handleChange={handleChange} />
-      <SelectComp title="Estado" options={["Bastante usada","Pouco usada", "Semi nova"]} name="estado" handleChange={handleChange} />
+      <InputComp nameprop="cidade" label="Cidade" type="text" place="" value={handleChange} />
+      <SelectComp title="Estado" options={["RS","SP"]} name="estado" handleChange={handleChange} />
       </Stack>
       <InputComp nameprop="logradouro" label="Logradouro" type="text" place="" value={handleChange} />
       <Stack direction={{ base: "column", md: "row" }}>
       <InputComp nameprop="numero" label="Número" type="text" place="" value={handleChange} />
       <InputComp nameprop="complemento" label="Complemento" type="text" place="" value={handleChange} />
       </Stack>
-
-      <Box>
-        <Text fontWeight="500">Adicione uma foto da roupa!</Text>
-        <FormLabel textAlign="center" borderWidth=".1rem" borderColor="brand.btn" borderRadius="md" borderStyle="dashed" p={5} cursor="pointer">
-          <Icon as={MdCloudUpload} fontSize="5rem" textAlign="center" cursor="pointer" transition=".3s" _hover={{ color: "brand.btn" }} />
-          <Input
-            type="file"
-            disabled={uploadStatus ? true : null}
-            nameprop="file"
-            accept="image/png, image/jpeg"
-            border="none"
-            display="none"
-            name="pictures"
-            onChange={handleChanges}
-          />
-          <Text>Selecione um arquivo (jpeg, jpg, png)</Text>
-        </FormLabel>
-        <Box>
-          <HStack justifyContent="center">
-            {uploadStatus ? (
-              <Spinner />
-            ) : (
-              urlLink.map((data) => (
-                <VStack p={3} key={nanoid()}>
-                  <Text>{data.imageData}</Text>
-                  <Box w="70px" h="80px" overflow="hidden">
-                    <Image loading="lazy" src={data.urlLink} borderRadius="md" objectFit="cover" w="100%" />
-                  </Box>
-                </VStack>
-              ))
-            )}
-          </HStack>
-          <Text textAlign="center" as="p" fontSize=".9rem" color="brand.description">
-            Imagens anexadas: {urlLink.length} / 3
-          </Text>
-        </Box>
-        <FormLabel>
-        <InputComp nameprop="descricao_post" label="Descrição" type="text" place="" value={handleChange} />
-        </FormLabel>
-      </Box>
       {toSubmit ? <Confetti /> : null}
       <Button role="button" disabled={uploadStatus && true} type="submit" mt={4} p={2} w="100%" bg="brand.btn" color="brand.bg" _hover={{ bg: "#789b8b" }}>
         {toSubmit ? (
           <>
             <HStack alignItems={"center"}>
               <Text as="p" fontSize={{ base: ".9rem", md: "1rem" }}>
-                Obrigado por sua doação!
+                Salvo!
                 <span role="img" aria-labelledby="emoji-hearth">
                   🤗
                 </span>{" "}
@@ -105,7 +52,7 @@ const Form = () => {
             </HStack>
           </>
         ) : (
-          "Publicar!"
+          "Salvar"
         )}
       </Button>
       {redirect && (
